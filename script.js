e4d90d4781002f5a5a4b21bc2260a75721e9409e
oliimendor <endor.sensey@gmail.com>
2024-12-28 12:49:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let ism = prompt("Ismingizni kiriting: ")
+let id = prompt("idingizni lkiriting")
+let password = prompt("parolingizni kiriting ")
+let obj = {
+    ism: ism,
+    id: id
+}
+console.log(obj);
